2ee07fe1bcbbd364c3229e68f1bfdc8ea4a9b411
TOGEP <sg.endo0302@gmail.com>
2021-12-09 21:06:43 +0900

    update: response json

--- main.go
+++ main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
+type Item struct {
+	Topic string `json:"topic"`
+	Data  int    `json:"data"`
+}
+
 func main() {
+	//TODO db init
 	mux := http.NewServeMux()
 	mux.Handle("/temperature", http.HandlerFunc(temperature))
 	log.Println(http.ListenAndServe(":8080", mux))
 }
 
-func temperature(w http.ResponseWriter, _ *http.Request) {
+func temperature(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getTemperature(w, r)
+	case http.MethodPost:
+		addTemperature(w, r)
+	default:
+		// 現状その他は想定していない
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
+
+func getTemperature(w http.ResponseWriter, r *http.Request) {
+	item := Item{http.MethodGet, http.StatusOK}
+	res, err := json.Marshal(item)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "24.00")
+	w.Write(res)
+}
+
+func addTemperature(w http.ResponseWriter, r *http.Request) {
+	item := Item{http.MethodPost, http.StatusOK}
+	res, err := json.Marshal(item)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
